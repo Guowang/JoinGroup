a2a826190301d62c063be8821d78ceff2b908d04
Guowang Li <lgwster@gmail.com>
2013-02-18 08:42:14 -0500

    Initial Commit

---
+++ JoinGroup/JGFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGFirstViewController.h
+//  JoinGroup
+//
+//  Created by user on 2/18/13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGFirstViewController : UIViewController
+
+@end
---
+++ JoinGroup/JGSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  JGSecondViewController.h
+//  JoinGroup
+//
+//  Created by user on 2/18/13.
+//  Copyright (c) 2013 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JGSecondViewController : UIViewController
+
+@end
